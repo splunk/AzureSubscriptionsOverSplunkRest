f91c48c07fe81ef94418d1a29d5ea28d093ad3b3
ahoang-splunk <106760044+ahoang-splunk@users.noreply.github.com>
2023-01-12 16:20:29 -0500

    Update postAzureSubscriptions.py

--- postAzureSubscriptions.py
+++ postAzureSubscriptions.py
@@ -1,23 +1,16 @@
 import requests
-
-def postSubscriptions(subscriptionList):
+from requests.auth import HTTPBasicAuth 
+def postSubscriptions(splunk_web_user, splunk_web_pass, subscriptionList):
     
     url = "https://localhost:8089/servicesNS/nobody/TA-MS-AAD/TA_MS_AAD_azure_subscription?output_mode=json"
-    print(subscriptionList)
+    basicauth = HTTPBasicAuth(splunk_web_user, splunk_web_pass)
+    
+
     if (len(subscriptionList) != 0):
         for sub in subscriptionList:
             payload='name='+str(sub)+'&interval=86400&index=default&azure_app_account=AzureAccount&tenant_id=123456&environment=public&source_type=azure%3Asubscriptions'
-            headers = {
-            'Accept': 'application/json, text/plain, */*',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Authorization': 'Basic YWRtaW46U3BsdW5rLjU=',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Cookie': 'splunkd_8000=QIWr8JZo47hkfEw1TmMVHv_zoUGZWVdg0I7lTgTXz8NMObgA4WiLQkBA7wTyDmZpClCq26wdztfdYTr9^dO11tipGo2G_clOdEIpxpVk01QP7zIGFsXYXuy_xLCzXrzKZ7wq; splunkweb_csrf_token_8000=15658481517227332700'
-            }
-
-            response = requests.request("POST", url, headers=headers, data=payload, verify=False)
-            response = response.text 
+            response = requests.post(url, auth=basicauth, data=payload, verify=False).json()
             print(response)
     return "Success"
 
-print(postSubscriptions(['Subscription6', 'Subscription7']))
+print(postSubscriptions('admin','Splunk.5',['Subscription8']))
