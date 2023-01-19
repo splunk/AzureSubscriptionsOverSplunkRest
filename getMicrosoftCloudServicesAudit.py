67c3088f578930774e4301df2d36b0676e9134a7
ahoang-splunk <106760044+ahoang-splunk@users.noreply.github.com>
2023-01-19 17:29:25 -0500

    Update getMicrosoftCloudServicesAudit.py

--- getMicrosoftCloudServicesAudit.py
+++ getMicrosoftCloudServicesAudit.py
@@ -2,14 +2,13 @@ import requests
 from requests.auth import HTTPBasicAuth 
 def postSubscriptions(splunk_web_user, splunk_web_pass):
     
-    url = "https://10.202.12.146:8089/services/data/inputs/all?search=mscs_azure_audit&output_mode=json"
+    url = "https://10.202.12.146:8089/servicesNS/nobody/Splunk_TA_microsoft-cloudservices/splunk_ta_mscs_mscs_azure_audit?output_mode=json&count=-1"
     basicauth = HTTPBasicAuth(splunk_web_user, splunk_web_pass)
     
 
     response = requests.get(url, auth=basicauth, verify=False).json()
     for name in response['entry']:
-        print(name)
-        print('\n')
+        print(name, '\n')
 
     return ('Success')
 
